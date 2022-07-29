ef34338ecbb9aaf28644d7853573604b5331194d
Pablo Carranza Vélez <pablo@edgeandnode.com>
2022-07-29 16:13:48 +0200

    fix: only call drip if enough time has passed

--- call_reservoir_drip/src/index.ts
+++ call_reservoir_drip/src/index.ts
@@ -1,7 +1,8 @@
 // Import dependencies available in the autotask environment
 import { RelayerParams } from 'defender-relay-client/lib/relayer'
 import { DefenderRelayProvider, DefenderRelaySigner } from 'defender-relay-client/lib/ethers'
-import { ethers } from 'ethers'
+import { ethers, BigNumber } from 'ethers'
+import { Relayer } from 'defender-relay-client'
 
 // Import an ABI which will be embedded into the generated js
 import L1ReservoirAbi from '@graphprotocol/contracts/dist/abis/L1Reservoir.json'
@@ -15,18 +16,31 @@ type EventWithParameters= {
   signer?: ethers.providers.JsonRpcSigner | null
 }
 
+type RelayerOrSigner = ethers.providers.JsonRpcSigner | DefenderRelaySigner
 type AutotaskEvent = RelayerParams | EventWithParameters
 
+async function latestBlock(signer: RelayerOrSigner, relayer: Relayer | null): Promise<BigNumber> {
+  let block
+  if (relayer) {
+    block = await relayer.call('eth_blockNumber', [])
+  } else [
+    block = await (signer.provider as ethers.providers.JsonRpcProvider).send('eth_blockNumber', [])
+  ]
+  console.log(block)
+  return BigNumber.from(block)
+}
 // Entrypoint for the Autotask
 export async function handler(event: AutotaskEvent) {
+  const minInterval = (6 * 24 * 60 * 60) / 12 // 6 days, in post-Merge blocks
   const credentials: RelayerParams = event as unknown as RelayerParams
-  let signer: DefenderRelaySigner | ethers.providers.JsonRpcSigner
+  let signer: RelayerOrSigner
   let params = event as EventWithParameters
-
+  let relayer: Relayer | null
   if (params.signer) {
     signer = params.signer as ethers.providers.JsonRpcSigner
   } else {
     const provider = new DefenderRelayProvider(credentials)
+    relayer = new Relayer(credentials)
     signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' })
   }
   
@@ -43,12 +57,19 @@ export async function handler(event: AutotaskEvent) {
   const l1ReservoirAddress: string = (addressBook as any)[chainId]['L1Reservoir'].address
   const l1Reservoir = new ethers.Contract(l1ReservoirAddress, L1ReservoirAbi, signer) as unknown as L1Reservoir
   const issuanceRate = await l1Reservoir.issuanceRate()
-  console.log(`L1Reservoir issuanceRate is ${issuanceRate}. Dripping...`)
+  console.log(`L1Reservoir issuanceRate is ${issuanceRate}.`)
 
-  const tx = await l1Reservoir['drip(uint256,uint256,uint256,address)'](0, 0, 0, providerAddress)
-  const receipt = await tx.wait()
-  console.log(`dripped, tx: ${receipt.transactionHash}`)
-
+  const currentBlock = await latestBlock(signer, relayer)
+  const lastDripBlock = await l1Reservoir.lastRewardsUpdateBlock()
+  console.log(`Current block ${currentBlock}, last drip at ${lastDripBlock}`)
+  if (currentBlock.sub(lastDripBlock.toString()).gt(minInterval)) {
+    console.log(`Dripping using signer ${providerAddress}`)
+    const tx = await l1Reservoir['drip(uint256,uint256,uint256,address)'](0, 0, 0, providerAddress)
+    const receipt = await tx.wait()
+    console.log(`dripped, tx: ${receipt.transactionHash}`)
+  } else {
+    console.log('A drip happened recently, skipping.')
+  }
 }
 
 // Sample typescript type definitions
